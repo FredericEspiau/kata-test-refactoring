587ee0aac0057fd3a4a8c63bb4955f7c9ca396dc
Frédéric Espiau <frederic.espiau@gmail.com>
2020-09-07 22:48:36 +0200

    added the code for the kata

---
+++ src/address.ts
@@ -0,0 +1,9 @@
+export class Address {
+  constructor(
+    public streetName: string,
+    public first: string,
+    public second: string,
+    public address: string,
+    public country: string
+  ) {}
+}
